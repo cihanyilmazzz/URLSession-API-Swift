9dfc9bfff8c47f383e9fc450fa63ed6ab58caf44
Cihan Yilmaz <chnrzgr@gmail.com>
2025-08-08 20:12:20 +0200

    Initial Commit

---
+++ URLSession-Api/URLSession_ApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSession_ApiApp.swift
+//  URLSession-Api
+//
+//  Created by Cihan Yilmaz on 08/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct URLSession_ApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ URLSession-ApiTests/URLSession_ApiTests.swift
@@ -0,0 +1,17 @@
+//
+//  URLSession_ApiTests.swift
+//  URLSession-ApiTests
+//
+//  Created by Cihan Yilmaz on 08/08/2025.
+//
+
+import Testing
+@testable import URLSession_Api
+
+struct URLSession_ApiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
